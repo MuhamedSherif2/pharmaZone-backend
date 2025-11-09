8a5ae36b0936237c9a4301630a8d13d22f1c517b
ayayasser01 <ayayasser920@gmail.com>
2025-11-09 22:50:07 +0200

    add user

--- models/user.model.js
+++ models/user.model.js
@@ -1,1 +1,13 @@
-sdfasdfasdfasd
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: String },
+  address: { type: String },
+  createdAt: { type: Date, default: Date.now },
+isDeleted:{type:Boolean,default:false}
+});
+
+module.exports = mongoose.model("User", userSchema);
