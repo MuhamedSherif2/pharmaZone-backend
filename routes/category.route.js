8e5ce832ac7380e97e8185b8b990a59aa72afe20
asmaa <asmaasalehnoon@gmail.com>
2025-12-21 16:10:39 +0200

    updates

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 import upload from '../middleWars/upload.middleware.js';
-
+import { authenticate, authorize } from "../middleWars/auth.middlewar.js";
 import {
   createCategory,
   getAllCategories,
@@ -15,8 +15,8 @@ router.post("/",upload.single("imageUrl"), createCategory);
 router.get("/", getAllCategories);
 router.get("/:id", getCategoryById);
 router.put("/:id", upload.single("imageUrl"),updateCategory);
-router.delete("/:id", deleteCategory);
-router.patch("/restore/:id", restoreCategory);
+router.delete("/:id", authenticate,authorize('pharmacy'),deleteCategory);
+router.patch("/restore/:categoryId",authenticate,authorize('pharmacy'), restoreCategory);
 
 
 export default router;
